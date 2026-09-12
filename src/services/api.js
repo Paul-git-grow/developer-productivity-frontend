d6e4d06d7c5bd5333bda3c28ae5886a82f5be9ab
paul7558 <paulpauls7558@gmail.com>
2026-09-12 16:50:35 +0530

    add api

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL:
+    "https://developer-productivity-backend-cr79.onrender.com/api",
 });
 
 API.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token =
+      localStorage.getItem("token");
 
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
+      config.headers.Authorization =
+        `Bearer ${token}`;
     }
 
     return config;
